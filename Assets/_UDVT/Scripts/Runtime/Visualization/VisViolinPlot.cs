2739934f9dd993a34140a3b171e6014871c13b48
Priscille <zranneu.sei@student.uni-siegen.de>
2023-07-12 15:23:08 +0200

    changed Shape of densityPlot and boxplot

--- Assets/_UDVT/Scripts/Runtime/Visualization/VisViolinPlot.cs
+++ Assets/_UDVT/Scripts/Runtime/Visualization/VisViolinPlot.cs
@@ -38,13 +38,17 @@ public class VisViolinPlot : Vis
 
         // Extract the x and y values from the calculated_points
         double[] calculatedPoints_XValues = new double[nsteps];
-        double[] calculatedPoints_YValues = new double[nsteps];
+        double[] calculatedPoints_YValues = new double[2*nsteps];
         for (int i = 0; i < nsteps; i++)
         {
             calculatedPoints_XValues[i] = calculated_points[i, 0];
+            
             calculatedPoints_YValues[i] = calculated_points[i, 1];
+
+            //set the negative y values to be able to represent the rotated continuous distribution in the Grid XY.
+            calculatedPoints_YValues[calculatedPoints_YValues.Length - 1 - i] = -calculatedPoints_YValues[i]; 
         }
-       
+
         // Create Axes and Grids
         // Call the base class method to create the visualization container
         base.CreateVis(container);
@@ -66,7 +70,7 @@ public class VisViolinPlot : Vis
 
         // Plot all calculated points
         // Create data marks using the data mark prefab
-        visContainer.CreateDataMarks(dataMarkPrefab);/**/
+        visContainer.CreateDataMarks(dataMarkPrefab);
 
 
         // Create a line renderer to draw the line connecting the data marks
@@ -78,7 +82,7 @@ public class VisViolinPlot : Vis
         // Get the positions of the data marks and set them as the positions for the line renderer
         var marks = visContainer.dataMarkList;
         Vector3[] positions = new Vector3[marks.Count];
-        double[] data_x = new double[marks.Count]; //scaled x points
+        double[] data_x = new double[marks.Count]; // set the scaled x points for calculating or getting the quartiles (median, 25%, 75%), maximum and minimum values.
         for (int i = 0; i < marks.Count; ++i)
         {
             positions[i] = marks[i].GetDataMarkChannel().position;
@@ -123,40 +127,22 @@ public class VisViolinPlot : Vis
         
         violinContainer = new GameObject("ViolinContainer");
         violinContainer.transform.SetParent(container.transform);
-        // // Create quartile mark
-        medianMark = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        // quartileMark.name = "QuartileMark";
+
+         // create new GameObjects for drawing the quartiles (median, 25%, 75%), maximum and minimum
+        medianMark = new GameObject("medianMark");       
         medianMark.transform.SetParent(violinContainer.transform);
-        medianMark.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);//new Vector3((float)statistics.getIQR(), (float)statistics.getIQR(), (float)statistics.getIQR());
-        medianMark.transform.position = new Vector3((float)statistics.getMedian(), 0.1f, 0f);
-        
-
-        // // Create min/max mark
-        minMark = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        // minMaxMark.name = "MinMaxMark";
+              
+        minMark = new GameObject("minMark");
         minMark.transform.SetParent(violinContainer.transform);
-        minMark.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
-        minMark.transform.position = new Vector3((float)statistics.getMinValue(), 0.1f, 0f);
         
-        maxMark = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        maxMark.transform.SetParent(violinContainer.transform);
-        maxMark.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
-        maxMark.transform.position = new Vector3((float)statistics.getMaxValue(), 0.1f, 0f);
+        maxMark = new GameObject("MaxMark");
+        maxMark.transform.SetParent(container.transform);
         
-        q3Mark = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        q3Mark.transform.SetParent(violinContainer.transform);
-        q3Mark.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
-        q3Mark.transform.position = new Vector3((float)statistics.getUpperQuartile(),0.1f, 0f);
+        q3Mark = new GameObject("Q3Mark");
+        q3Mark.transform.SetParent(container.transform);
 
-        q1Mark = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        q1Mark = new GameObject("Q1Mark");;
         q1Mark.transform.SetParent(violinContainer.transform);
-        q1Mark.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
-        q1Mark.transform.position = new Vector3( (float)statistics.getLowerQuartile(), 0.1f,0f);
-        
-        /*iqrMark = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        iqrMark.transform.SetParent(violinContainer.transform);
-        iqrMark.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
-        iqrMark.transform.position = new Vector3((float)statistics.getIQR(), (float)statistics.getIQR(), 0f);*/
   
 
          Debug.Log("Median: " + statistics.getMedian());
@@ -166,7 +152,53 @@ public class VisViolinPlot : Vis
          Debug.Log("Min Value: " + statistics.getMinValue());
          Debug.Log("Max Value: " + statistics.getMaxValue());
 
+        Vector3[] lineUpperQuartileMaxValue = new Vector3[2] { new Vector3((float)statistics.getUpperQuartile(), 0.5f, 0), new Vector3((float)statistics.getMaxValue(), 0.5f, 0)};
+        LineRenderer lineRenderer2 = violinContainer.AddComponent<LineRenderer>();
+        lineRenderer2.startWidth = 0.005f;
+        lineRenderer2.endWidth = 0.005f;
+        lineRenderer2.material.color = Color.blue;
+        lineRenderer2.positionCount = 2;
+        lineRenderer2.SetPositions(lineUpperQuartileMaxValue);
 
+        Vector3[] lineMinValueLowerQuartile = new Vector3[2] { new Vector3((float)statistics.getLowerQuartile(), 0.5f, 0), new Vector3((float)statistics.getMinValue(), 0.5f, 0)};
+        LineRenderer lineMinValueLowerQuartileRender = q1Mark.AddComponent<LineRenderer>();
+        lineMinValueLowerQuartileRender.startWidth = 0.005f;
+        lineMinValueLowerQuartileRender.endWidth = 0.005f;
+        lineMinValueLowerQuartileRender.material.color = Color.blue;
+        lineMinValueLowerQuartileRender.positionCount = 2;
+        lineMinValueLowerQuartileRender.SetPositions(lineMinValueLowerQuartile);
+
+        
+        Vector3[] lineMaxValue = new Vector3[2] { new Vector3((float)statistics.getMaxValue(), 0.515f, 0), new Vector3((float)statistics.getMaxValue(), 0.485f, 0f)};
+        LineRenderer lineRenderer3 = maxMark.AddComponent<LineRenderer>();
+        lineRenderer3.startWidth = 0.005f;
+        lineRenderer3.endWidth = 0.005f;
+        lineRenderer3.positionCount = 2;
+        lineRenderer3.SetPositions(lineMaxValue);
+
+        Vector3[] violinBox = new Vector3[4] { new Vector3((float)statistics.getLowerQuartile(), 0.480f, 0), new Vector3((float)statistics.getLowerQuartile(), 0.520f, 0f), 
+            new Vector3((float)statistics.getUpperQuartile(), 0.520f, 0f),  new Vector3((float)statistics.getUpperQuartile(), 0.480f, 0f)};
+        LineRenderer lineviolinBox = q3Mark.AddComponent<LineRenderer>();
+        lineviolinBox.startWidth = 0.005f;
+        lineviolinBox.endWidth = 0.005f;
+        lineviolinBox.loop=true;
+        lineviolinBox.positionCount = 4;
+        lineviolinBox.SetPositions(violinBox);
+
+        Vector3[] medianLine = new Vector3[2] { new Vector3((float)statistics.getMedian(), 0.520f, 0f),  new Vector3((float)statistics.getMedian(), 0.480f, 0f)};
+        LineRenderer medianLineRender = medianMark.AddComponent<LineRenderer>();
+        medianLineRender.startWidth = 0.005f;
+        medianLineRender.endWidth = 0.005f;
+        medianLineRender.positionCount = 2;
+        medianLineRender.SetPositions(medianLine);
+
+        Vector3[] minValueLine = new Vector3[2] { new Vector3((float)statistics.getMinValue(), 0.515f, 0f),  new Vector3((float)statistics.getMinValue(), 0.485f, 0f)};
+        LineRenderer minValueLineRender = minMark.AddComponent<LineRenderer>();
+        minValueLineRender.startWidth = 0.005f;
+        minValueLineRender.endWidth = 0.005f;
+        minValueLineRender.positionCount = 2;
+        minValueLineRender.SetPositions(minValueLine);
+        
         return visContainerObject;
     }
 }
